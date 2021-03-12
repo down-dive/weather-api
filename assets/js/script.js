002af98bfe07fd6483641b9ceb81183ed2eb05b0
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-03-12 12:11:58 -0800

    fetching information from first current weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
-// let getUserInfo = () => {
-//     fetch("https://api.openweathermap.org/data/2.5/onecall?lat").then((response) => {
-//         response.json().then((data) => {
-//             console.log(data);
-//         })
-//     });
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+
+
+let getUserInfo = () => {
+    fetch("https:api.openweathermap.org/data/2.5/weather?q=Sunnyvale,94087&appid=b20a0c8394f0e460f879d6303c6f83ca").then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+        })
+    });
    
-//    };
-//    getUserInfo();
-
-var response = fetch("https://api.openweathermap.org/data/2.5/onecall?lat");
-console.log(response);
-
-console.log("one")
+   };
+   getUserInfo();
